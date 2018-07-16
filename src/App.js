4ab248caf5642def0e1d5169783f75b3dfc6dbc0
Temo Ojeda <ojeda.temo@gmail.com>
2018-07-15 20:00:36 -0700

    grid

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo_2 from './paragraph.png'
+
+import { Grid } from 'semantic-ui-react'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {/* Columns have to add up to 16 in one row */}
+        <Grid container columns={2}>
+          <Grid.Row>
+            {/* Menu column */}
+            <Grid.Column mobile={16} tablet={8} computer={4}>
+              <h1>MENU</h1>
+              <img src={logo_2} alt="logo_2" />
+            </Grid.Column>
+            {/* Map/Info column */}
+            <Grid.Column mobile={16} tablet={8} computer={12}>
+              <h1>Map or whatever</h1>
+              <img src={logo_2} alt="logo_2" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
+
     );
   }
 }
